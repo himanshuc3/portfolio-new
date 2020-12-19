3ce7211d38d88368cc6d250cd1cfef52c95d42cb
Himanshu <himanshu.chhabra@rapyuta-robotics.com>
2020-12-19 20:10:22 +0530

    feat: About page with header and footer

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -7,6 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPageComponent implements OnInit {
 
+  aboutMeList: any = [
+    {
+      title: 'I love experimenting with different backend languages \n (currently on go)',
+      path: 'assets/experiment.svg'
+    },
+    {
+      title: 'Setup includes linux (ubuntu), terminator, vscode',
+      path: 'assets/terminal.svg'
+    },
+    {
+      title: 'Chess enthusiast, blunder master, hate being flagged',
+      path: 'assets/flag.svg'
+    },
+    {
+      title: 'Lover of sunshine, not to be mistaken with climate change',
+      path: 'assets/sun.svg'
+    },
+    {
+      title: 'Hit me up on fortnite, cod, rocket league',
+      path: 'assets/gamepad.svg'
+    }
+    
+  ]
+
+  musicAlbums = [
+    {
+      path: 'assets/eminem_kamikaze.png',
+      artist: 'Eminem',
+      name: 'Kamikaze'
+    },
+     {
+      path: 'assets/coldplay_everydaylife.png',
+      artist: 'Coldplay',
+      name: 'Everyday life'
+    },
+     {
+      path: 'assets/eminem_kamikaze.png',
+      artist: 'Eminem',
+      name: 'Kamikaze'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { BlogPageComponent } from './blog-page/blog-page.component';
 import LandingPage from './landing-page/landing-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import LandingPage from './landing-page/landing-page.component';
     HeaderComponent,
     AboutPageComponent,
     BlogPageComponent,
-    LandingPage ,
+    LandingPage,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
