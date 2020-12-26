d06d6bd6eb4721e5317e911aff02b7cc873530f4
Himanshu <himanshu.chhabra@rapyuta-robotics.com>
2020-12-26 15:25:37 +0530

    feat: landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export default class LandingPage implements OnInit {
 
+  recentBlogs:any = [
+    {
+      title: 'On hover show poster',
+      path: 'assets/react.svg',
+      description: 'While working on pointclouds for Amphion and Zethus, I came across an interesting problem.',
+      date: 'Dec 09, 2020'
+    },
+    {
+      title: 'The Import On Interaction Pattern',
+      path: 'assets/recently-2.svg',
+      description: 'While working on pointclouds for Amphion and Zethus, I came across an interesting problem.',
+      date: 'Dec 09, 2020'
+    },
+    {
+      title: 'The Import On Interaction Pattern',
+      path: 'assets/react.svg',
+      description: 'While working on pointclouds for Amphion and Zethus, I came across an interesting problem.',
+      date: 'Dec 09, 2020'
+    },
+  ]
+
+  projects = [
+    {
+      title: 'cric-cli',
+      description: 'A cli extension to follow cricket in cli made in Node js and nothing to be worry off.',
+      github: 'https://www.github.com/rishabh570/criccli',
+      path: 'assets/github-logo.svg'
+    },
+    {
+      title: 'cric-cli',
+      description: 'A cli extension to follow cricket in cli made in Node js and nothing to be worry off.',
+      github: 'https://www.github.com/rishabh570/criccli',
+      path: 'assets/github-logo.svg'
+
+    },
+    {
+      title: 'cric-cli',
+      description: 'A cli extension to follow cricket in cli made in Node js and nothing to be worry off.',
+      github: 'https://www.github.com/rishabh570/criccli',
+      path: 'assets/github-logo.svg'
+
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
