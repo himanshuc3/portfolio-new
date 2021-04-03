28b07b2250c535b0cd7a6c90a173fc44a760ad0b
Himanshu <himanshu.chhabra@rapyuta-robotics.com>
2021-04-03 14:58:38 +0530

    correct routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AboutPageComponent } from './about-page/about-page.component';
 import LandingPage from './landing-page/landing-page.component';
 
 
-const routes: Routes = [{ path: '', component: LandingPage},{ path: 'about', component: AboutPageComponent},  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
+const routes: Routes = [{ path: 'about', component: AboutPageComponent},  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },{ path: '**', component: LandingPage},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
